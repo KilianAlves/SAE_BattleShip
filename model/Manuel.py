0a5b41ff24e51c5ed74296372ef8f82c8d787af6
KilianAlves <alveskilian@gmail.com>
2022-01-06 15:36:41 +0100

    Ecriture des fonctions repondreTirJoueur et marquerCouleGrille

--- model/Manuel.py
+++ model/Manuel.py
@@ -1,6 +1,9 @@
 # model/Manuel.py
 #
+from model.Case import type_case
 from model.Constantes import *
+from model.Coordonnees import type_coordonnees
+from model.Grille import marquerCouleGrille
 from model.Joueur import type_joueur
 from view import window
 
@@ -17,7 +20,7 @@ def placerBateauxManuel(joueur:dict) -> None:
 def choisirCaseTirManuel(joueur:dict) -> tuple:
 
     if not type_joueur(joueur):
-        raise ValueError("choisirCaseTirManuel : ")
+        raise ValueError(f"choisirCaseTirManuel : erreur {joueur} n'est pas un joueur")
 
 
     window.afficher(joueur)
@@ -25,4 +28,23 @@ def choisirCaseTirManuel(joueur:dict) -> tuple:
     window.set_action(f"Choisissez la case de tir (bouton gauche)")
     res = window.get_clicked_cell(2)
 
-    return res
+    return res[0]
+
+
+def traiterResultatTirManuel(joueur:dict,coord:tuple,rep:str) -> None:
+
+    if not type_joueur(joueur):
+        raise ValueError(f"traiterResultatTirManuel : erreur {joueur} n'est pas un joueur")
+    if not type_coordonnees(coord):
+        raise ValueError(f"traiterResultatTirManuel : erreur {coord} n'est pas une coordonnée")
+    if None in coord:
+        raise ValueError(f"traiterResultatTirManuel : erreur {coord} est Null")
+    if not type_case(rep):
+        raise ValueError(f"traiterResultatTirManuel : erreur {rep} n'est pas un str")
+
+    joueur[const.JOUEUR_GRILLE_TIRS][coord[0]][coord[1]] = rep
+    if rep == const.COULE:
+        marquerCouleGrille(joueur[const.JOUEUR_GRILLE_TIRS],coord)
+    print(rep)
+
+    return None
